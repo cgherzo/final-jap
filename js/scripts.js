a568d81d520e831a7a533f2c7000a33f39a0f191
Carolina Gherzo <carolinagherzo@gmail.com>
2017-11-10 10:36:47 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+ // JQuery de datetimepicker
+ $(function () {
+ 	$('#datetimepicker1').datetimepicker();
+  });
+
+function selectService(selectedValue){
+var e = document.getElementById("serviceSelected");
+e.value = selectedValue;
+};
